85c3372d2da6197a5e91d8a96960ff0042d0d565
Jon Brick <jonbrick09@gmail.com>
2023-07-16 16:34:58 -0400

    starting creating layout section

---
+++ components/theme-toggle.tsx
@@ -0,0 +1,27 @@
+"use client" 
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+export function ThemeToggle() {
+    const { setTheme, theme} = useTheme();
+
+    return (
+        <button
+            className="relative inline-flex"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+            <Sun
+                size="16"
+                className="trainstion-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0"
+            >
+            </Sun>
+            <Moon
+                size="16"
+                className="absolute transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100"
+            > 
+            </Moon> 
+            <span className="sr-only">Toggle theme</span>
+        </button>
+    );
+}
